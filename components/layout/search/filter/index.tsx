70355ef4209f4d5fa56f6129dda8c3ba5f549752
Trịnh Long Vũ <81180330+trinhvu1711@users.noreply.github.com>
2024-03-11 21:52:11 +0700

    create dropdown
    add dropdown with category, collection, sort by in dropdown

---
+++ components/layout/search/filter/index.tsx
@@ -0,0 +1,42 @@
+import { SortFilterItem } from "@/lib/constants";
+import { FilterItem } from "./item";
+import FilterItemDropdown from "./dropdown";
+
+export type ListItem = SortFilterItem | PathFilterItem;
+export type PathFilterItem = { title: string; path: string };
+
+function FilterItemList({ list }: { list: ListItem[] }) {
+  return (
+    <>
+      {list.map((item: ListItem, i) => (
+        <FilterItem key={i} item={item} />
+      ))}
+    </>
+  );
+}
+
+export default function FilterList({
+  list,
+  title,
+}: {
+  list: ListItem[];
+  title?: string;
+}) {
+  return (
+    <>
+      <nav>
+        {title ? (
+          <h3 className="hidden text-xs text-neutral-500 md:block dark:text-neutral-400">
+            {title}
+          </h3>
+        ) : null}
+        <ul className="hidden md:block">
+          <FilterItemList list={list} />
+        </ul>
+        <ul className="hidden">
+          <FilterItemDropdown list={list} />
+        </ul>
+      </nav>
+    </>
+  );
+}
