6c688acf5803e7db007cb071dfe2bee270f92f2c
Trịnh Long Vũ <81180330+trinhvu1711@users.noreply.github.com>
2024-05-19 22:35:38 +0700

    add tracking order

---
+++ app/order/tracking/[slug]/page.tsx
@@ -0,0 +1,7 @@
+import { getOrderByTrackingNumber } from "@/lib/services/order";
+import TrackingOrder from "./TrackingOrder";
+
+export default async function page({ params }: { params: { slug: string } }) {
+  const order = await getOrderByTrackingNumber(params.slug);
+  return <TrackingOrder order={order} />;
+}
